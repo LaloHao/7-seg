017eb5e390bf9ec8d33649e6a64af7b05f1d30b1
Eduardo Vazquez <lalohao@gmail.com>
2014-10-15 22:15:48 -0500

    First version

---
+++ msg.h
@@ -0,0 +1,39 @@
+/*
+    7-seg
+    Copyright (C) 2014  Eduardo Vazquez (LaloHao)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define MSG_BIENVENIDA "7-seg"
+#define MSG_TABLAS "[Tablas de verdad]"
+#define MSG_SEGMENTOS "[Segmentos]"
+#define MSG_PAUSE "Presione una tecla para continuar . . ."
+#define MSG_PEDIR_SEGMENTO "Dame el segmento(a-g) ('h' para ayuda): "
+#define MSG_CAMBIAR_DATO "Indique el dato a cambiar: "
+#define MSG_DATO_INCORRECTO "El dato no existe."
+#define MSG_CAMBIAR_INDICE "Indique el nuevo indice: "
+#define MSG_INDICE_INCORRECTO "Indice incorrecto."
+#define MSG_ANODE "Anodo"
+#define MSG_CATHODE "Catodo"
+#define MSG_AYUDA "\
+[Comandos]\n \
+z : salir del programa \n \
+h : ayuda \n \
+x : cambiar el dato actual a modificar\n \
+t : mostrar tabla de datos \n \
+i : cambiar indice\n \
+k : activar/desactivar catodo comun\n "
+#define EXIT_SUCCESS 0
+#define MAX 25
