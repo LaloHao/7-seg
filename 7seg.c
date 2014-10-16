05069eaa8919f22702ca134ea0cad91bfb1fee6f
Eduardo Vazquez <lalohao@gmail.com>
2014-10-16 00:31:01 -0500

    Added formula for calculating how many displays are on screen

--- 7seg.c
+++ 7seg.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 25 //How many displays do you want?
+#define SPANISH //Comment line if you want english
+#define MAX 13 /*How many displays do you want?
+ Default console is 80x24, this means you can print up to (80/6)=13
+ displays without resizing your window*/
 #include "kernel.c"
 
 int main(void)
